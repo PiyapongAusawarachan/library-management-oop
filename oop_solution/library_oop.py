62c25f1b67e55f671dbac8a62f81674d76c38ec9
PiyapongAusawarachan <piyapong.au@ku.th>
2025-11-13 23:28:29 +0700

    Third commit when the second class is developed and tested

--- oop_solution/library_oop.py
+++ oop_solution/library_oop.py
@@ -17,3 +17,31 @@ class Book:
             self.available += 1
             return True
         return False
+
+
+class Member:
+    def __init__(self, mid, fullname, mail="N/A"):
+        self.mid = mid
+        self.fullname = fullname
+        self.mail = mail
+        self.books = []
+
+    def borrow_book(self, bk):
+        if len(self.books) >= 3:
+            print("Error: Borrow limit reached!")
+            return False
+        if not bk.borrow_copy():
+            print("Error: No copies left!")
+            return False
+        self.books.append(bk)
+        print(f"{self.fullname} borrowed '{bk.title}'")
+        return True
+
+    def return_book(self, bk):
+        if bk not in self.books:
+            print("Error: Book not borrowed!")
+            return False
+        self.books.remove(bk)
+        bk.return_copy()
+        print(f"{self.fullname} returned '{bk.title}'")
+        return True
--- oop_solution/test_oop.py
+++ oop_solution/test_oop.py
@@ -1,13 +1,16 @@
-from library_oop import Book
+from library_oop import Book, Member
 
-b = Book(1, "Python 101", "John Doe", 3)
+b1 = Book(1, "Python 101", "John", 2)
+b2 = Book(2, "C++ Basics", "Jane", 1)
+m = Member(1, "Alice", "alice@mail.com")
 
-print(b.available)
-b.borrow_copy()
-print(b.available)
-b.borrow_copy()
-print(b.available)
-b.return_copy()
-print(b.available)
-b.return_copy()
-print(b.available)
+print(b1.available)
+m.borrow_book(b1)
+print(b1.available)
+m.borrow_book(b2)
+print(b2.available)
+m.return_book(b1)
+print(b1.available)
+m.borrow_book(b1)
+m.borrow_book(b1)
+m.borrow_book(b1)
